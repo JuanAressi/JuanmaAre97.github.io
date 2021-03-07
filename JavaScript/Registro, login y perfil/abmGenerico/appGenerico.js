344b16fcff5f57f3a532433a8937f2b581a3de1f
Juancho <Juan.Aressi@hotmail.com>
2021-03-07 12:25:42 -0300

    Commit para Gabi

--- JavaScript/Registro, login y perfil/abmGenerico/appGenerico.js
+++ JavaScript/Registro, login y perfil/abmGenerico/appGenerico.js
@@ -1,30 +1,16 @@
 window.onload = function() {
-    let tablaABM = document.getElementById('tablaABM');
-    let titulo = "Titulo2";
-    let lista = [
-        {
-            "id": "0",
-            "nombre": "juan"
-        },
-        {
-            "id": "1",
-            "nombre": "seba"
-        },
-        {
-            "id": "2",
-            "nombre": "gabi"
-        },
-        {
-            "id": "3",
-            "nombre": "roman"
-        }
-    ];
-    let titulos = ["id", "nombre"];
+    let body = document.querySelector('body');
+    
+    let titulo = "Lista de Autos";
+
+    let lista = getFromLS('listaAutos');
+
+    let titulosTabla = ['Marca', 'Modelo', 'Año', 'Color', 'Patente'];
 
     const funcionEspecifica = () => {
         // console.log('llamada');
         alert('llamada generica');
     }
 
-    renderizarHTML(tablaABM, titulo, lista, titulos, funcionEspecifica);
+    renderizarHTML(body, titulo, lista, titulosTabla, funcionEspecifica);
 }
--- JavaScript/Registro, login y perfil/componentes/listaABM.js
+++ JavaScript/Registro, login y perfil/componentes/listaABM.js
@@ -1,22 +1,36 @@
-function renderizarHTML(tablaABM, titulo, lista, arrayTitulos, funcionEspecifica) {
-    tablaABM.innerHTML += getTemplate(titulo, arrayTitulos, lista, funcionEspecifica);    
+function renderizarHTML(body, titulo, lista, atributos, funcionEspecifica) {
+    body.innerHTML += getTemplate(titulo, atributos, lista, funcionEspecifica);    
 }
 
-function getTemplate(titulo, arrayTitulos, lista, funcionEspecifica) {
+function getTemplate(titulo, atributos, lista, funcionEspecifica) {
     let texto = '';
     let list = '';
 
-    for (let i = 0; i < arrayTitulos.length; i++) {
+    for (let i = 0; i < atributos.length; i++) {
         texto += `
-            <th scope="col">${arrayTitulos[i]}</th>            
+            <th scope="col">${atributos[i]}</th>            
         `;
     }
     
     lista.forEach(item => {
-        list += `
+        // let list = '';
+
+        // let tr = document.createElement('tr');
+        list += `  
             <tr>
-                <td>${item.id}</td>            
-                <td>${item.nombre}</td>            
+        `;
+
+        for (let i = 0; i < atributos.length; i++) {
+            list += `
+                <td>${item[atributos[i]]}</td>
+            `;
+        }
+
+        // tr.innerHTML += list;
+
+        // document.querySelector('tbody').innerHTML += tr;
+
+        list += `
             </tr>
         `;
     });
@@ -25,27 +39,27 @@ function getTemplate(titulo, arrayTitulos, lista, funcionEspecifica) {
     var info = `
     <div class="container bg-white p-5">
         <div class="d-flex justify-content-between align-items-center mb-4">
-            <button id="backToPerfil" onclick="clickB1(${funcionEspecifica});">
-                <i class="far fa-arrow-left">Botton 1</i>
+            <button id="backToPerfil" class="btn btn-secondary" onclick="clickB1(${funcionEspecifica});" >
+                <i class="far fa-arrow-left"></i>
             </button>
 
             <h1 class="text-center">${titulo}</h1>
 
-            <button id="addCar" onclick="clickB2()">
-                Boton 2
+            <button id="addCar" class="btn btn-success" onclick="clickB2()">
+                <i class="far fa-plus"></i>
             </button>
         </div>
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                ${texto}
-            </tr>
-        </thead>
-        <tbody>
-            ${list}
-        </tbody>
-    </table>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    ${texto}
+                </tr>
+            </thead>
+            <tbody>
+                ${list}
+            </tbody>
+        </table>
     </div>
 `;
 
